d5a6676df18615f2fdfd5f873c3229e507e74a50
a4084461 <a4084461@gmail.com>
2025-08-20 14:05:14 +0800

    generate dumy controller

---
+++ Together.Api/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Together.Api.Controllers;
+
+[ApiController]
+[Route("api")]
+public class EventController : ControllerBase
+{
+
+    [Route("add-event")]
+    public IActionResult AddEvent()
+    {
+        return Ok();
+    }
+
+    [Route("query-event")]
+    public IActionResult QueryEvent()
+    {
+        return Ok();
+    }
+
+}
