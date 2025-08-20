12491d589680ed92d07e427415c7c77d83dae7bb
a4084461 <a4084461@gmail.com>
2025-08-20 16:06:44 +0800

    DI Injection at api

--- Together.Api/Controllers/EventController.cs
+++ Together.Api/Controllers/EventController.cs
@@ -1,22 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
+using Together.Application.Services;
+using Together.Contract.Controllers;
 
 namespace Together.Api.Controllers;
 
 [ApiController]
 [Route("api")]
-public class EventController : ControllerBase
+public class EventController(IEventService eventService) : ControllerBase
 {
 
-    [Route("add-event")]
-    public IActionResult AddEvent()
+    private readonly IEventService eventService = eventService;
+
+    [HttpPost("add-event")]
+    public IActionResult AddEvent(AddEventRequest request)
     {
-        return Ok();
+        AddEventResponse res = new(
+            Guid.NewGuid(),
+            request.name,
+            request.coordinator,
+            request.place,
+            request.lat,
+            request.lng,
+            request.fee
+            );
+        return Ok(res);
     }
 
-    [Route("query-event")]
-    public IActionResult QueryEvent()
+    [HttpPost("query-event")]
+    public IActionResult QueryEvent(QueryEventRequest request)
     {
-        return Ok();
+        QueryEventResponse res = new(
+            "activity name",
+            "Mark Ho",
+            "Taipei city hall",
+            23.8f,
+            100.4f,
+            500
+         );
+        QueryEventResponse[] events = [res, res, res];
+        return Ok(events);
     }
 
 }
